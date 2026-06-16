cedcb416ea5836ba71f222dbdfb0ba11999d25f7
K001-code <kin993341@gmail.com>
2026-06-16 09:45:41 +0700

    Create main.py
    First version of the format of the program, its functions and variables.

---
+++ main.py
@@ -0,0 +1,22 @@
+#This is the main branch where the final version of the code will be merged into after revision
+import cv2, face_recognition, sys
+
+def main():
+  """
+  This program utalizes the openCV/cv2 open Python library to capture, recognize student's face via a webcam.
+  Once a student's face is detected in the trained data, we can automatically update a spreadsheet that contains all the students' attendance status.
+  """
+  ret, frame = webCam()
+  if not ret:
+    sys.exit("ERROR: Something went wrong while trying to capture your webcam.")
+  drawRect(frame)
+  
+  return 0
+
+
+def webCam():
+  cap = cv2.VideoCapture(0)
+  return cap.read()
+
+if __name__ == "__main__":
+  main()

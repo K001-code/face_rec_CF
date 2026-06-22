91a84f197c88bf5b1ccec143b2af352f11a678fc
to_bota <tobota86@gmail.com>
2026-06-22 10:32:26 +0700

    Add face detection rectangle

--- main.py
+++ main.py
@@ -9,28 +9,42 @@ def main():
     cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
-        # 0, 1 
+
         if not ret:
             sys.exit("ERROR: Something went wrong while trying to capture your webcam.")
+
+        drawRect(frame)   # <-- put it here
+
         cv2.imshow("Camera", frame)
+
         if cv2.waitKey(1) == ord("q"):
             break
+
     cap.release()
     cv2.destroyAllWindows()
     
-    # drawRect(frame)
     
     return 0
 
 
 def drawRect(frame):
-    #your code here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
     return
 
 if __name__ == "__main__":
     main()
 
-
-#ababababbabab
-
-# jg mech mel ey 
